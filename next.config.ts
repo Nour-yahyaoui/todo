4f4c7036ad798b71c91addcad4edc1e44ae46f36
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-02-08 14:35:51 +0100

    update next

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-   images: {
-    domains: [
-      'images.unsplash.com',
-      'plus.unsplash.com',
-      'source.unsplash.com',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
